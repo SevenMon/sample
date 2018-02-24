2572d7aae0cea0cae4a7e26d05ad00400a3a351f
SevenMon <57867782@qq.com>
2018-02-24 06:40:00 +0000

    finish login logout

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class SessionsController extends Controller
 {
@@ -14,11 +15,36 @@ class SessionsController extends Controller
     //登录操作
     public function store(Request $request)
     {
-       $credentials = $this->validate($request, [
-           'email' => 'required|email|max:255',
-           'password' => 'required'
-       ]);
 
-       return;
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt($credentials,$request->has('remember'))) {
+            // 该用户存在于数据库，且邮箱和密码相符合
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        }else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back();
+        }
+        return;
+    }
+    //退出
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+    //检查登录
+    public function checklogin(){
+        var_dump(Auth);
+        exit();
+        if(Auth::check()){
+            echo "yijingdenglu";
+        }else {
+            echo "no denglu";
+        }
     }
 }
