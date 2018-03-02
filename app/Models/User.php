5d9cd28cae84604796ce17e3f7d4723b1ac57353
SevenMon <57867782@qq.com>
2018-03-02 06:57:23 +0000

    add user following

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Notifications\ResetPassword;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -36,10 +37,42 @@ class User extends Authenticatable
         });
     }
 
-
+    // 对应关系  一个用户对应多个 statuses 文章
     public function statuses()
     {
         return $this->hasMany(Status::class);
+    }
+    //对应关系  一个用户对应多个粉丝(用户)
+    //自己对粉丝(查看自己有多少个粉丝)
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+    //粉丝对自己(查看自己关注了多少个人)关注某人
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 
     public function sendPasswordResetNotification($token)
@@ -53,7 +86,10 @@ class User extends Authenticatable
     }
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids, Auth::user()->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
 }
